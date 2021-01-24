7238381f4a76059aeefd7b409d812084c5c04d1f
OZALTIN <OZALTIN@DESKTOP-8G1NH3A>
2021-01-24 16:59:11 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Kurs";
+            string kurs2 = "Programlamaya Giriş";
+            string kurs3 = "Java";
+            string kurs4 = "Pyhton";
+            string kurs5 = "C++";
+
+            //aray - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Kurs", "Programlamaya Giriş", "Java", 
+                "Pyhton", "C++"};
+
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("Sayfa Sonu - footer");
+
+        }
+    }
+}
---
+++ YGKampı/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace YGKampı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butobu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
